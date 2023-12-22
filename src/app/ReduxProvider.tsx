bb78043b517d45d0d04dda161f25963a4959b39c
ShuvoProgram <shuvoprogramer@gmail.com>
2023-12-22 18:07:29 +0600

    Add ReduxProvider component 🚀

---
+++ src/app/ReduxProvider.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
+type Props = {
+  children: ReactNode;
+};
+
+const ReduxProvider = ({ children }: Props) => {
+  return <Provider store={store}>{children}</Provider>;
+};
+
+export default ReduxProvider;
