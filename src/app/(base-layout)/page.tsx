0bc3dc0f47b8a20439c2c03dcd64a9e81f52a3d1
ShuvoProgram <shuvoprogramer@gmail.com>
2023-12-22 18:11:10 +0600

    Added client connection, imported Meta, and set up Learnify details 🚀📚

---
+++ src/app/(base-layout)/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+import Meta from "../utils/Meta";
+
+const Page = () => {
+  return (
+    <div>
+      <Meta
+        title="Learnify"
+        description="Discover Learnify: Your Key to Convenient and Engaging Online Learning. Explore a Diverse Range of Courses and Empower Your Educational Journey with Our User-Friendly, Modern eLearning Platform."
+        keywords="Online Learning, E-Learning, Skill Development, Programming, ReactJs, NodeJs, MERN"
+      />
+    </div>
+  );
+};
+
+export default Page;
