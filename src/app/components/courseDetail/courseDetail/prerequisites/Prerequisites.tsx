34e0c488bffcfe11f8a08d916d63078d6ac5c42f
ShuvoProgram <shuvoprogramer@gmail.com>
2023-12-30 11:37:02 +0600

    Added prerequisites display 🛠️ in React component.

---
+++ src/app/components/courseDetail/courseDetail/prerequisites/Prerequisites.tsx
@@ -0,0 +1,21 @@
+import { IoCheckmarkDoneOutline } from "react-icons/io5";
+
+const Prerequisites = ({ prerequisites }: any) => {
+  return (
+    <div>
+      {prerequisites?.map((item: any, index: number) => (
+        <div className="w-full flex 800px:items-center py-2" key={index}>
+          <div className="w-[15px] mr-1">
+            <IoCheckmarkDoneOutline
+              size={20}
+              className="text-black dark:text-white"
+            />
+          </div>
+          <p className="pl-2 text-black dark:text-white">{item?.title}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Prerequisites;
