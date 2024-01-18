8acde30013eadbd745b0b5bda8a7dd13a674cc07
ShuvoProgram <shuvoprogramer@gmail.com>
2024-01-18 11:28:46 +0600

    Add Footer component with EduSphere information and links 🌐

--- src/app/(base-layout)/layout.tsx
+++ src/app/(base-layout)/layout.tsx
@@ -2,7 +2,6 @@
 import Header from "../components/Header";
 import { ReactNode } from "react";
 import Footer from "../components/Home/footer/Footer";
-import FooterDemo from "../components/Home/footer/FooterDemo";
 
 type Props = {
   children: ReactNode;
@@ -14,8 +13,7 @@ export default function RootLayout({ children }: Props) {
       <Header />
       <div>{children}</div>
       <div>
-        {/* <Footer/> */}
-        <FooterDemo/>
+        <Footer/>
       </div>
     </>
   );
