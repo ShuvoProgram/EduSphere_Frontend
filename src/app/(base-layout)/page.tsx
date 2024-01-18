71571ac0afe4f5bf04fcac7289d0ec318ff81af2
ShuvoProgram <shuvoprogramer@gmail.com>
2024-01-18 12:24:30 +0600

    Add motion animation to Courses component 🚀

--- src/app/(base-layout)/page.tsx
+++ src/app/(base-layout)/page.tsx
@@ -2,7 +2,6 @@
 import Achievement from "../components/Home/Achivement/Achivement";
 import Banner from "../components/Home/Banner/Banner";
 import Categories from "../components/Home/Categories/Categories";
-import Companies from "../components/Home/Companies/Companies";
 import Courses from "../components/Home/Course/Courses";
 import Partners from "../components/Home/Partners/Partners";
 import Testimonial from "../components/Home/Testimonial/Testimonial";
@@ -18,9 +17,10 @@ const Page = () => {
         keywords="Online Learning, E-Learning, Skill Development, Programming, ReactJs, NodeJs, MERN"
       />
       <Banner/>
-      <Companies/>
-      <Categories/>
+      {/* <Companies/> */}
+   
       <Courses/>
+      <Categories/>
       <Achievement/>
       <Testimonial/>
       <Partners/>
