b68f7ce158aa788a67cbda59720db88931bf52d1
ShuvoProgram <shuvoprogramer@gmail.com>
2024-01-19 18:36:51 +0600

    Add user analytics page with Meta component

---
+++ src/app/(admin-layout)/admin/users-analytics/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import Meta from "../../../utils/Meta";
+import UsersAnalytics from "../../../components/admin/users-analytics/UsersAnalytics";
+
+const page = () => {
+  return (
+    <div>
+      <Meta title="User Analytics - EduSphere" />
+      <UsersAnalytics />
+    </div>
+  );
+};
+
+export default page;
