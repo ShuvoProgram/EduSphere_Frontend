aa18785dc6af5a8aa2969a000665956d3944ed34
ShuvoProgram <shuvoprogramer@gmail.com>
2024-01-19 11:19:56 +0600

    Add TermsConditions component with content

---
+++ src/app/(base-layout)/terms-conditions/page.tsx
@@ -0,0 +1,14 @@
+import TermsConditions from '@/app/components/TermsConditions/TermsConditions';
+import Meta from '@/app/utils/Meta';
+import React from 'react';
+
+function page() {
+  return (
+    <div>
+      <Meta title="TermsConditions - EduSphere" />
+      <TermsConditions/>
+    </div>
+  );
+}
+
+export default page;
---
+++ src/app/components/TermsConditions/TermsConditions.tsx
@@ -0,0 +1,105 @@
+import React from 'react';
+
+function TermsConditions() {
+  return (
+    <section className="pt-20 pb-20">
+      <div className="container mx-4">
+        <div className="main-content-text">
+          <h3 className="text-2xl font-bold mb-4">1. Accuracy and validity of information</h3>
+          <p className="mb-6">
+            Proin eget tortor risus. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Quisque velit nisi,
+            pretium ut lacinia in elementum id enim. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi.
+            Cras ultricies ligula sed magna dictum porta. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus.
+            Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Donec rutrum congue leo eget malesuada.
+            Donec rutrum congue leo eget malesuada. Pellentesque in ipsum id orci porta dapibus. Curabitur aliquet quam id
+            ut lacinia in elementum id enim. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi leo eget malesuada.
+          </p>
+
+          <div className="my-8"></div>
+
+          <h3 className="text-2xl font-bold mb-4">2. Availability</h3>
+          <p className="mb-6">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, odio sequi! Nam consectetur, officiis ipsam
+            explicabo tempore placeat! Voluptas molestiae pariatur necessitatibus consectetur, ullam aut esse amet debitis sint
+            culpa. orem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, odio Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Vitae mollitia laborum assumenda beatae minima enim quis dolores, pariatur, at perferendis sit
+            dolore modi nostrum eum ea voluptatem
+          </p>
+          <p className="mb-6">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut, placeat? Culpa ab est nemo perspiciatis quam, nesciunt
+            reprehenderit voluptate id error corrupti doloremque exercitationem quis, iusto debitis velit eveniet ea. ipsum
+            dolor sit amet consectetur, adipisicing elit. Ut, placeat? Culpa ab est nemo perspiciatis quam, nesciunt reprehenderit
+          </p>
+
+          <div className="my-8"></div>
+
+          <h3 className="text-2xl font-bold mb-4">3. Third party websites</h3>
+          <p className="mb-6">
+            Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Vivamus suscipit tortor eget felis porttitor
+            volutpat. Nulla quis lorem ut libero malesuada feugiat. Nulla porttitor accumsan tincidunt. Sed porttitor lectus nibh.
+            Pellentesque in ipsum id orci porta dapibus.Nulla quis lorem ut libero malesuada feugiat. Proin eget tortor risus.
+            Nulla porttitor accumsan tinci dunt. Donec rutrum congue leo eget malesuada. Vestibulum ac diam sit amet quam vehicula
+            elementum sed sit amet dui. Vivamus magna justo.
+          </p>
+          <p className="mb-6">
+            Quisque velit nisi, pretium ut lacinia in, elementum id enim. Proin eget tortor risus. Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit. Curabitur aliquet quam id dui posuere blandit. Vivamus magna justo, lacinia eget
+            consectetur sed, convallis at tellus. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi.
+          </p>
+
+          <div className="my-8"></div>
+
+          <h3 className="text-2xl font-bold mb-4">4. Copyright and intellectual property</h3>
+          <p className="mb-6">
+            Nulla quis lorem ut libero malesuada feugiat. Proin eget tortor risus. Nulla porttitor accumsan tincidunt. Donec rutrum
+            congue leo eget malesuada. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Vivamus magna justo,
+            lacinia eget consectetur sed, convallis at tellus libero malesuada feugiat. Proin eget tortor.
+          </p>
+          <ul className="list-disc list-inside mb-6">
+            <li className="mb-2">
+              <i className="ri-check-line inline-block mr-2"></i>
+              Quisque velit nisi, pretium ut lacinia in, elementum id enim.
+            </li>
+            <li className="mb-2">
+              <i className="ri-check-line inline-block mr-2"></i>
+              Proin eget tortor risus consectetur adipiscing elit.
+            </li>
+            <li className="mb-2">
+              <i className="ri-check-line inline-block mr-2"></i>
+              Curabitur aliquet quam id dui posuere blandit.
+            </li>
+          </ul>
+
+          <div className="my-8"></div>
+
+          <h3 className="text-2xl font-bold mb-4">6. Termination of contract</h3>
+          <p className="mb-6">
+            Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Vivamus suscipit tortor eget felis porttitor
+            volutpat. Nulla quis lorem ut libero malesuada feugiat. Nulla porttitor accumsan tincidunt. Sed porttitor lectus nibh.
+            Pellentesque in ipsum id orci porta dapibus.Nulla quis lorem ut libero malesuada feugiat. Proin eget tortor risus.
+            Nulla porttitor accumsan tinci dunt. Donec rutrum congue leo eget malesuada. Vestibulum ac diam sit amet quam vehicula
+            elementum sed sit amet dui. Vivamus magna justo
+          </p>
+          <p className="mb-6">
+            Quisque velit nisi, pretium ut lacinia in, elementum id enim. Proin eget tortor risus. Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit. Curabitur aliquet quam id dui posuere blandit. Vivamus magna justo, lacinia eget
+            consectetur sed, convallis at tellus. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi.
+          </p>
+
+          <div className="my-8"></div>
+
+          <h3 className="text-2xl font-bold mb-4">7. Limitation of liability</h3>
+          <p className="mb-6">
+            Proin eget tortor risus. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Quisque velit nisi, pretium ut
+            lacinia in elementum id enim. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Cras ultricies
+            ligula sed magna dictum porta. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vestibulum ac diam
+            sit amet quam vehicula elementum sed sit amet dui. Donec rutrum congue leo eget malesuada. Donec rutrum congue leo eget
+            malesuada. Pellentesque in ipsum id orci.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default TermsConditions;
