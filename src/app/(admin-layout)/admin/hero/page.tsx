811f7d6c14beb44d5de27e8cf67ef0c7b7b8cdc7
ShuvoProgram <shuvoprogramer@gmail.com>
2024-01-19 18:25:41 +0600

    Add/Edit hero page with Meta title and component import

---
+++ src/app/(admin-layout)/admin/hero/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import Meta from "../../../utils/Meta";
+import EditHero from "../../../components/admin/customization/EdtHero";
+
+const page = () => {
+  return (
+    <div>
+      <Meta title="Edit Hero - EduSphere" />
+      <EditHero />
+    </div>
+  );
+};
+
+export default page;
