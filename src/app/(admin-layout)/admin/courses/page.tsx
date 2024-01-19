59b95a41f2caf6d028965e3ef9b3ec3592892263
ShuvoProgram <shuvoprogramer@gmail.com>
2024-01-19 18:17:57 +0600

    Add Courses page with Meta and AllCourses components

---
+++ src/app/(admin-layout)/admin/courses/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import Meta from "../../../utils/Meta";
+import AllCourses from "../../../components/admin/course/AllCourses";
+
+const page = () => {
+  return (
+    <div>
+      <Meta title="Courses - EduSphere" />
+      <AllCourses />
+    </div>
+  );
+};
+
+export default page;
