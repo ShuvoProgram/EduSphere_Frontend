aa5005feeaceedc3dcbd0fa7463ff0b673d4fca2
ShuvoProgram <shuvoprogramer@gmail.com>
2024-01-19 18:23:10 +0600

    Add EditCoursePage component with Meta and EditCourse imports

---
+++ src/app/(admin-layout)/admin/edit-course/[id]/page.tsx
@@ -0,0 +1,13 @@
+import Meta from "../../../../utils/Meta";
+import EditCourse from "../../../../components/admin/course/EditCourse";
+
+const EditCoursePage = () => {
+  return (
+    <div>
+      <Meta title="Edit Course - EduSphere" />
+      <EditCourse />
+    </div>
+  );
+};
+
+export default EditCoursePage;
