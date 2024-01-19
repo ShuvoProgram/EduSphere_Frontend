8192f17a2b1ede9dd78b13f50691b6ca17470bbc
ShuvoProgram <shuvoprogramer@gmail.com>
2024-01-19 18:24:30 +0600

    Add/Edit client page with Meta title and EditFaq component

---
+++ src/app/(admin-layout)/admin/faq/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import Meta from "../../../utils/Meta";
+import EditFaq from "../../../components/admin/customization/EditFaq";
+
+const page = () => {
+  return (
+    <div>
+      <Meta title="Edit FAQ - EduSphere" />
+      <EditFaq />
+    </div>
+  );
+};
+
+export default page;
