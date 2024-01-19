0c5d9a57458c2a42ed09250823b6835b5d828253
ShuvoProgram <shuvoprogramer@gmail.com>
2024-01-19 18:21:58 +0600

    Add CreateCoursePage component with Meta and CreateCourse imports

---
+++ src/app/(admin-layout)/admin/create-course/page.tsx
@@ -0,0 +1,13 @@
+import Meta from "../../../utils/Meta";
+import CreateCourse from "../../../components/admin/course/CreateCourse";
+
+const CreateCoursePage = () => {
+  return (
+    <div>
+      <Meta title="Create Course - EduSphere" />
+      <CreateCourse />
+    </div>
+  );
+};
+
+export default CreateCoursePage;
