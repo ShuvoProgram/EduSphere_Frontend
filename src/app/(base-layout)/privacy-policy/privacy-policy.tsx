88f34c3941b3254efcf50605ed53eccea9d0729b
ShuvoProgram <shuvoprogramer@gmail.com>
2024-01-21 01:35:05 +0600

    Add privacy policy component

---
+++ src/app/(base-layout)/privacy-policy/privacy-policy.tsx
@@ -0,0 +1,139 @@
+import React from "react";
+
+const privacyPolicy = () => {
+  return (
+    <>
+      <section className="pt-16 pb-24">
+        <div className="container mx-auto">
+          <div className="main-content-text">
+            <h3 className="text-2xl font-bold mb-2">1. Personal information:</h3>
+            <p className="mb-4">
+              Proin eget tortor risus. Mauris blandit aliquet elit, eget
+              tincidunt nibh pulvinar a. Quisque velit nisi, pretium ut
+              lacinia in elementum id enim. Praesent sapien massa, convallis a
+              pellentesque nec, egestas non nisi. Cras ultricies ligula sed
+              magna dictum porta. Curabitur non nulla sit amet nisl tempus
+              convallis quis ac lectus. Vestibulum ac diam sit amet quam
+              vehicula elementum sed sit amet dui. Donec rutrum congue leo eget
+              malesuada. Donec rutrum congue leo eget malesuada. Pellentesque
+              in ipsum id orci porta dapibus. Curabitur aliquet quam id ut
+              lacinia in elementum id enim. Praesent sapien massa, convallis a
+              pellentesque nec, egestas non nisi leo eget malesuada.
+            </p>
+
+            <div className="mt-8"></div>
+
+            <h3 className="text-2xl font-bold mb-2">
+              2. Collecting personal information:
+            </h3>
+            <p className="mb-4">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Assumenda, odio sequi! Nam consectetur, officiis ipsam explicabo
+              tempore placeat! Voluptas molestiae pariatur necessitatibus
+              consectetur, ullam aut esse amet debitis sint culpa. orem ipsum
+              dolor sit amet consectetur adipisicing elit. Assumenda, odio
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
+              mollitia laborum assumenda beatae minima enim quis dolores,
+              pariatur, at perferendis sit dolore modi nostrum eum ea
+              voluptatem
+            </p>
+            <p className="mb-4">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut,
+              placeat? Culpa ab est nemo perspiciatis quam, nesciunt
+              reprehenderit voluptate id error corrupti doloremque
+              exercitationem quis, iusto debitis velit eveniet ea. ipsum dolor
+              sit amet consectetur, adipisicing elit. Ut, placeat? Culpa ab est
+              nemo perspiciatis quam, nesciunt reprehenderit
+            </p>
+
+            <div className="mt-8"></div>
+
+            <h3 className="text-2xl font-bold mb-2">
+              3. Using and disclosing your personal information:
+            </h3>
+            <p className="mb-4">
+              Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem.
+              Vivamus suscipit tortor eget felis porttitor volutpat. Nulla
+              quis lorem ut libero malesuada feugiat. Nulla porttitor accumsan
+              tincidunt. Sed porttitor lectus nibh. Pellentesque in ipsum id
+              orci porta dapibus.Nulla quis lorem ut libero malesuada feugiat.
+              Proin eget tortor risus. Nulla porttitor accumsan tinci dunt.
+              Donec rutrum congue leo eget malesuada. Vestibulum ac diam sit
+              amet quam vehicula elementum sed sit amet dui. Vivamus magna
+              justo.
+            </p>
+            <p className="mb-4">
+              Quisque velit nisi, pretium ut lacinia in, elementum id enim.
+              Proin eget tortor risus. Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit. Curabitur aliquet quam id dui posuere blandit.
+              Vivamus magna justo, lacinia eget consectetur sed, convallis at
+              tellus. Praesent sapien massa, convallis a pellentesque nec,
+              egestas non nisi.
+            </p>
+
+            <div className="mt-8"></div>
+
+            <h3 className="text-2xl font-bold mb-2">4. Contact by the company:</h3>
+            <p className="mb-4">
+              Nulla quis lorem ut libero malesuada feugiat. Proin eget tortor
+              risus. Nulla porttitor accumsan tincidunt. Donec rutrum congue
+              leo eget malesuada. Vestibulum ac diam sit amet quam vehicula
+              elementum sed sit amet dui. Vivamus magna justo, lacinia eget
+              consectetur sed, convallis at tellus libero malesuada feugiat.
+              Proin eget tortor.
+            </p>
+            <ul className="list-disc pl-5 mb-4">
+              <li>Quisque velit nisi, pretium ut lacinia in, elementum id enim.</li>
+              <li>Proin eget tortor risus consectetur adipiscing elit.</li>
+              <li>Curabitur aliquet quam id dui posuere blandit.</li>
+            </ul>
+
+            <div className="mt-8"></div>
+
+            <h3 className="text-2xl font-bold mb-2">
+              5. Individual’s right of access:
+            </h3>
+            <p className="mb-4">
+              Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem.
+              Vivamus suscipit tortor eget felis porttitor volutpat. Nulla
+              quis lorem ut libero malesuada feugiat. Nulla porttitor accumsan
+              tincidunt. Sed porttitor lectus nibh. Pellentesque in ipsum id
+              orci porta dapibus.Nulla quis lorem ut libero malesuada feugiat.
+              Proin eget tortor risus. Nulla porttitor accumsan tinci dunt.
+              Donec rutrum congue leo eget malesuada. Vestibulum ac diam sit
+              amet quam vehicula elementum sed sit amet dui. Vivamus magna
+              justo
+            </p>
+            <p className="mb-4">
+              Quisque velit nisi, pretium ut lacinia in, elementum id enim.
+              Proin eget tortor risus. Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit. Curabitur aliquet quam id dui posuere blandit.
+              Vivamus magna justo, lacinia eget consectetur sed, convallis at
+              tellus. Praesent sapien massa, convallis a pellentesque nec,
+              egestas non nisi.
+            </p>
+
+            <div className="mt-8"></div>
+
+            <h3 className="text-2xl font-bold mb-2">
+              6. The company and links to other web sites:
+            </h3>
+            <p className="mb-4">
+              Proin eget tortor risus. Mauris blandit aliquet elit, eget
+              tincidunt nibh pulvinar a. Quisque velit nisi, pretium ut
+              lacinia in elementum id enim. Praesent sapien massa, convallis a
+              pellentesque nec, egestas non nisi. Cras ultricies ligula sed
+              magna dictum porta. Curabitur non nulla sit amet nisl tempus
+              convallis quis ac lectus. Vestibulum ac diam sit amet quam
+              vehicula elementum sed sit amet dui. Donec rutrum congue leo eget
+              malesuada. Donec rutrum congue leo eget malesuada. Pellentesque
+              in ipsum id orci.
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default privacyPolicy;
