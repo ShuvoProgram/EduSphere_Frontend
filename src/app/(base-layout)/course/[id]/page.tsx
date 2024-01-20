18d10caecb334cdb7456b0472f5f73d26b2a0220
ShuvoProgram <shuvoprogramer@gmail.com>
2024-01-21 00:11:00 +0600

    added course details

---
+++ src/app/(base-layout)/course/[id]/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import Meta from "../../../utils/Meta";
+import CoursedetailPage from "../../../components/courseDetail//CoursedetailPage";
+
+const page = () => {
+  return (
+    <div>
+      <Meta title="Course Details - EduSphere" />
+      <CoursedetailPage />
+    </div>
+  );
+};
+
+export default page;
